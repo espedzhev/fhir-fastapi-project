df5765537ea19bd75e2dc7920173ebfb1556e364
espedzhev <evald.spedzhev@gmail.com>
2024-10-20 23:29:57 +0100

    added utils

--- app/main.py
+++ app/main.py
@@ -1,14 +1,10 @@
-import asyncio
-import os
 from contextlib import asynccontextmanager
-from pathlib import Path
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fhir.resources.R4B.bundle import Bundle
 
-from.routers import patients
-from app.db.redis import r
+from .routers import patients
+from .utils.lifespan import load_patient_data
 
 
 @asynccontextmanager
@@ -31,21 +27,3 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-async def load_patient_data(dataset_folder: str):
-    if r.exists('patients_loaded'):
-        return
-
-    for filename in os.listdir(dataset_folder):
-        file_path = Path(os.path.join(dataset_folder, filename))
-        bundle = Bundle.parse_file(file_path)
-
-        for entry in bundle.entry:
-            if entry.resource.resource_type == 'Patient':
-                patient = entry.resource
-
-                # await asyncio.to_thread(r.set, f"Bundle:{patient.id}", bundle.json())
-                await asyncio.to_thread(r.set, f"Patient:{patient.id}", patient.json())
-
-    await asyncio.to_thread(r.set, 'patients_loaded', 'true')
